b1e3a2afce8063833e4488891768a244a2e85dab
lms8262 <zergling8262@gmail.com>
2023-08-09 12:26:56 +0900

    first

---
+++ src/main/java/com/ezen/booktving/BooktvingApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.booktving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktvingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezen/booktving/BooktvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.booktving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
