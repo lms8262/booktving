f70fe0280ec11a9c2bff53283584f8bbeb407202
github dongjin1996 <github ehwl01@naver.com>
2023-08-14 16:34:44 +0900

    category

--- src/main/java/com/ezen/booktving/controller/CategoryController.java
+++ src/main/java/com/ezen/booktving/controller/CategoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CategoryController {
 	
 	//카테고리 메인 페이지 보여주기
-	@GetMapping(value = "/mainCategory")
+	@GetMapping(value = "/category/main")
 	public String MainCategory() {
 		
 		return "category/mainCategory";
@@ -15,7 +15,7 @@ public class CategoryController {
 	
 	
 	//서브 카테고리 페이지 보여주기
-	@GetMapping(value= "/subCategory")
+	@GetMapping(value= "/category/sub")
 	public String SubCategory() {
 		
 		return "category/subCategory";
