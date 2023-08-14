2f16030bc20bbef212388bd152f1c6d26e8263ea
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-08-14 15:18:05 +0900

    Update ApiService.java

--- src/main/java/com/ezen/booktving/service/ApiService.java
+++ src/main/java/com/ezen/booktving/service/ApiService.java
@@ -18,39 +18,33 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class ApiService {
-<<<<<<< HEAD
-	/*private final BookRepository bookRepository;
-=======
+
 	private final BestSellerRepository bestSellerRepository;
->>>>>>> 6b6b566495e6435166a315181c1b014c688d61f0
-
-    LocalDateTime dateTime = LocalDateTime.now();
-
-    public String getInfo(String result) throws ParseException {
-
-        JSONArray list = null;
-        
-        log.info("서비스 시작" );
-        JSONParser jsonParser = new JSONParser();
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
-        list = (JSONArray) jsonObject.get("item");
-
-        for (int k = 0; k < list.size(); k++) {
-        	JSONObject contents = (JSONObject) list.get(k);
-
-            bestSellerRepository.save(
-                    BestSeller.builder()
-                            .title(contents.get("title").toString())
-                            .isbn(contents.get("isbn").toString())
-                            .author(contents.get("author").toString())
-                            .publisher(contents.get("publisher").toString())
-                            .bestRank(contents.get("bestRank").toString())
-                            .link(contents.get("link").toString())
-                            .imgUrl(contents.get("cover").toString())
-                            .build()
-            );
-
-        }
-        return "ok";
-    }*/
+	
+   	public String getInfo(String result) throws ParseException {
+	        JSONArray list = null;
+	        
+	        log.info("서비스 시작" );
+	        JSONParser jsonParser = new JSONParser();
+	        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+	        list = (JSONArray) jsonObject.get("item");
+	
+	        for (int k = 0; k < list.size(); k++) {
+	        	JSONObject contents = (JSONObject) list.get(k);
+	
+	            bestSellerRepository.save(
+	                    BestSeller.builder()
+	                            .title(contents.get("title").toString())
+	                            .isbn(contents.get("isbn").toString())
+	                            .author(contents.get("author").toString())
+	                            .publisher(contents.get("publisher").toString())
+	                            .bestRank(contents.get("bestRank").toString())
+	                            .link(contents.get("link").toString())
+	                            .imgUrl(contents.get("cover").toString())
+	                            .build()
+	            );
+	
+	        }
+	        return "ok";
+    }
 }
