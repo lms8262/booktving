86fc43f6cb9a0b63f847c5865d4de1739606e7ca
lms8262 <130732104+lms8262@users.noreply.github.com>
2023-08-18 01:45:07 +0900

    Update BookRepository.java

--- src/main/java/com/ezen/booktving/repository/BookRepository.java
+++ src/main/java/com/ezen/booktving/repository/BookRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ezen.booktving.entity.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long>{
+public interface BookRepository extends JpaRepository<Book, Long>, BookRepositoryCustom{
 
 }
