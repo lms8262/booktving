7b776c97baf19a9a9caefecf045248e839fa4c01
lms8262 <zergling8262@gmail.com>
2023-08-18 18:20:42 +0900

    update apiService

--- src/main/java/com/ezen/booktving/entity/Book.java
+++ src/main/java/com/ezen/booktving/entity/Book.java
@@ -8,7 +8,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -17,6 +20,9 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book extends BaseTimeEntity {
 	
 	@Id
--- src/main/java/com/ezen/booktving/service/ApiService.java
+++ src/main/java/com/ezen/booktving/service/ApiService.java
@@ -1,14 +1,25 @@
 package com.ezen.booktving.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 
+import com.ezen.booktving.dto.BookSearchDto;
 import com.ezen.booktving.entity.BestSeller;
+import com.ezen.booktving.entity.Book;
 import com.ezen.booktving.entity.NewBookTving;
 import com.ezen.booktving.repository.BestSellerRepository;
+import com.ezen.booktving.repository.BookRepository;
 import com.ezen.booktving.repository.NewBookTvingRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,7 +30,8 @@ public class ApiService {
 
 	private final BestSellerRepository bestSellerRepository;
 	private final NewBookTvingRepository newBookTvingRepository;
-
+	private final BookRepository bookRepository;
+	
 	//main 페이지 - 서점 베스트셀러 api
     public String getBestSeller(String result) throws ParseException  {
 
@@ -74,5 +86,99 @@ public class ApiService {
     	return "ok";
     }
     
+    // 알라딘 검색 api -> 검색한 책 리스트 isbn만 가져오기
+    // 나중에 예외처리 해줘야함
+    public List<String> getSearchBookIsbnList(BookSearchDto bookSearchDto, int startIndex, int itemsPerPage) throws UnsupportedEncodingException, IOException, ParseException{
+    	String requestUrl = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
+    	String ttbKey = "ttblyczang41056001";
+    	String query = bookSearchDto.getSearchQuery();
+    	String queryType = bookSearchDto.getSearchBy();
+    	int start = startIndex;
+    	int maxResults = itemsPerPage;
+    	String cover = "Big";
+    	String output = "JS";
+    	String version = "20131101";
+    	
+    	StringBuilder sb = new StringBuilder();
+    	sb.append(requestUrl);
+    	sb.append("?TTBKey=");
+    	sb.append(ttbKey); // 필수
+    	sb.append("&Query=");
+    	sb.append(query); // 필수
+    	sb.append("&QueryType=");
+    	sb.append(queryType);
+    	sb.append("&Start=");
+    	sb.append(start);
+    	sb.append("&MaxResults=");
+    	sb.append(maxResults);
+    	sb.append("&Cover=");
+    	sb.append(cover);
+    	sb.append("&Output=");
+    	sb.append(output);
+    	sb.append("&Version=");
+    	sb.append(version);
+    	
+    	URL url = new URL(sb.toString());
+    	BufferedReader bf;
+    	bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+    	String result = bf.readLine();
+    	
+    	JSONArray list = null;
+    	JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+        list = (JSONArray) jsonObject.get("item");
+    	
+        List<String> searchBookIsbnList = new ArrayList<>();
+        for(int i = 0; i < list.size(); i++) {
+        	JSONObject contents = (JSONObject) list.get(i);
+        	String isbn13 = contents.get("isbn13").toString();
+        	if(isbn13 == null || isbn13 == "") { // isbn13 값 없으면 넘김
+        		continue;
+        	}
+        	searchBookIsbnList.add(isbn13);
+        }
+    	
+    	return searchBookIsbnList;
+    }
+    
+    // 알라딘 책 정보 조회 및 저장api
+    // 나중에 예외처리 해줘야함
+    public void getBookInfoByAladinApi(String isbn13) throws UnsupportedEncodingException, IOException, ParseException {
+    	String requestUrl = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx";
+    	String ttbKey = "ttblyczang41056001";
+    	String itemId = isbn13;
+    	String itemIdType = "ISBN13";
+    	String cover = "Big";
+    	String output = "JS";
+    	String version = "20131101";
+    	String optResult = "authors,Toc";
+    	
+    	StringBuilder sb = new StringBuilder();
+    	sb.append(requestUrl);
+    	sb.append("?TTBKey=");
+    	sb.append(ttbKey); // 필수
+    	sb.append("&ItemId=");
+    	sb.append(itemId); // 필수
+    	sb.append("&ItemIdType=");
+    	sb.append(itemIdType);
+    	sb.append("&Cover=");
+    	sb.append(cover);
+    	sb.append("&Output=");
+    	sb.append(output);
+    	sb.append("&Version=");
+    	sb.append(version);
+    	sb.append("&OptResult=");
+    	sb.append(optResult);
+    	
+    	URL url = new URL(sb.toString());
+    	BufferedReader bf;
+    	bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+    	String result = bf.readLine();
+    	
+    	JSONArray list = null;
+    	JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+    	// 여기 해야함
+    }
 }
 
