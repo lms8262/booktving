6f73091c98680b9d9fc5254a4e6f601307c1d699
lms8262 <zergling8262@gmail.com>
2023-08-29 10:54:37 +0900

    up

--- mysql.sql
+++ mysql.sql
@@ -2,10 +2,8 @@
 drop database booktving;
 CREATE DATABASE booktving DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-
 use booktving;
 select * from member;
-
 
 select * from best_seller;
 select * from new_book_tving;
